5a804944761f9076c0f0912c7f2cb6ea869ae12d
darshan <darshansalunke27@gmail.com>
2026-08-29 22:58:56 +0530

    n

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1dw&321x@b3nidc5(=(_(9!ty4h$9j$if6$#5@213p6i&+u9__'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-1dw&321x@b3nidc5(=(_(9!ty4h$9j$if6$#5@213p6i&+u9__')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
